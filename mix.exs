478ee3655b0c98dc5e46c3c7e5afe09fce8d99f6
lyokato <lyo.kato@gmail.com>
2017-09-23 11:00:37 +0000

    license

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Riverside.Mixfile do
     [app: :riverside,
      version: "0.1.0",
      elixir: "~> 1.4",
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -32,8 +33,22 @@ defmodule Riverside.Mixfile do
      {:ebus, "~> 0.2.1", hex: :erlbus},
      {:poison, "~> 3.1"},
      {:msgpax, "~> 2.0"},
+     {:ex_doc, "~> 0.15", only: :dev},
      {:graceful_stopper, github: "lyokato/graceful_stopper", tag: "0.1.1" },
      {:plug, "~> 1.3"}
     ]
   end
+
+  defp package() do
+    [
+      description: "A simple WebSocket server frame work.",
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/lyokato/riverside"
+        # "Docs" => "https://hexdocs.pm/riverside"
+      },
+      maintainers: ["Lyo Kato"]
+    ]
+  end
+
 end
