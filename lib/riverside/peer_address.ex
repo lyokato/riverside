a167d432271b01c7b87027f1f4ba5f2655d1b781
lyokato <lyo.kato@gmail.com>
2017-09-29 05:33:31 +0000

    changed: PeerAddress.address type

--- lib/riverside/peer_address.ex
+++ lib/riverside/peer_address.ex
@@ -1,21 +1,38 @@
 defmodule Riverside.PeerAddress do
 
+  @moduledoc ~S"""
+  Represents a peer's address
+
+  This data has following three field
+
+  * address
+  * port
+  * x_forwarded_for
+
+  """
+
   alias Riverside.Util.CowboyUtil
 
-  @type t :: %__MODULE__{address:         :inet.ip_address,
-                         port:            :inet.port_number,
+  @type t :: %__MODULE__{address: String.t,
+                         port: :inet.port_number,
                          x_forwarded_for: String.t}
 
-  defstruct address:         nil,
-            port:            0,
+  defstruct address: nil,
+            port: 0,
             x_forwarded_for: nil
+
+  @doc ~S"""
+  Pick a peer's address from a cowboy request.
+  """
+
+  @spec gather(:cowboy_req.req) :: t
 
   def gather(req) do
 
     {address, port, x_forwarded_for} = CowboyUtil.peer(req)
 
-    %__MODULE__{address:         address,
-                port:            port,
+    %__MODULE__{address: :inet_parse.ntoa(address),
+                port: port,
                 x_forwarded_for: x_forwarded_for}
 
   end
@@ -28,7 +45,7 @@ defimpl String.Chars, for: Riverside.PeerAddress do
 
   def to_string(%PeerAddress{address: address, port: port, x_forwarded_for: x_forwarded_for}) do
     Poison.encode!(%{
-      ip: "#{:inet_parse.ntoa(address)}",
+      ip: address,
       port: port,
       x_forwarded_for: x_forwarded_for
     })
