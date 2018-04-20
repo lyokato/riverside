09fb25df8619926f38fec22188e822da27a1fe29
lyokato <lyo.kato@gmail.com>
2018-04-20 03:35:53 +0000

    ver 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Riverside.Mixfile do
 
   def project do
     [app: :riverside,
-     version: "0.3.4",
+     version: "0.4.0",
      elixir: "~> 1.5",
      package: package(),
      build_embedded: Mix.env == :prod,
